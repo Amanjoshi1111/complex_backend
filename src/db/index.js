cd27a5dbb3ba263c99985265c4a26b051103ca2d
aman joshi <aman3.joshi@ril.com>
2024-04-30 13:14:54 +0530

    asyncHandler + ApiErrors + ApiResponse file created

--- src/db/index.js
+++ src/db/index.js
@@ -2,11 +2,11 @@ import mongoose from 'mongoose';
 import { DB_NAME } from '../constants.js';
 
 const connectDB = async ()=>{
-	try{
+	try{	
 		const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
 		console.log(`MONGODB CONNECTION FAILED !! DB HOST : ${connectionInstance.connection.host}`);
-	}catch(error){
-		console.error("Mongodb connection error ", error);
+	}catch(err){
+		console.error("Mongodb connection error ", err);
 		process.exit(1);
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import 'dotenv/config';
 import connectDB from "./db/index.js";
+import app from './app.js';
 
-connectDB();
+connectDB().
+	then(() => {
+		app.on('error', (err)=>{
+			console.log("ERROR : ", err);
+		})
+
+		app.listen(process.env.PORT || 8000, () => {
+			console.log(`SERVER LISTENING TO PORT ${process.env.PORT || 8000}`);
+		})
+	}).
+	catch((err) => {
+		console.log("MONGODB CONNECTION FAILED!!! ", err);
+	})
