46af35e12f29a350e3b32b196544e5f2ea1cd339
aman joshi <aman3.joshi@ril.com>
2024-05-14 19:13:52 +0530

    upload file(multer) add, project setup done

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -55,12 +55,12 @@ userSchema.pre("save", async function (next) {
 	if (!this.isModified("password"))
 		return next();
 
-	this.password = bcrypt.hash(this.password, 10);
+	this.password = await bcrypt.hash(this.password, 10);
 	next();
 })
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-	await bcrypt.compare(password, this.password);
+	return await bcrypt.compare(password, this.password);
 }
 
 userSchema.methods.generateAccessToken = async function () {
